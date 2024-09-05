911aefd6ebff5dfe60474644d0114778869b88ae
Mayara Bouwazra <mayarabouazra@gmail.com>
2024-09-05 19:29:58 +0100

    configure data

--- src/main/java/com/StockControlApp/Controllers/AdminController.java
+++ src/main/java/com/StockControlApp/Controllers/AdminController.java
@@ -1,4 +1,9 @@
 package com.StockControlApp.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Admin")
 public class AdminController {
 }
